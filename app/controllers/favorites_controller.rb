03393c87718193a1e3e0cd6b8194d56aa20d2d94
kazuma.koike <zns_re@yahoo.co.jp>
2020-08-18 12:03:04 +0000

    Debug

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,14 @@
 class FavoritesController < ApplicationController
   def create
-    user = User.find(params[:micropost_id])
-    current_user.favorite(user)
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite(micropost)
     flash[:success] = 'お気に入りに登録しました。'
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    user = User.find(params[:micropost_id])
-    current_user.unfavorite(user)
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unfavorite(micropost)
     flash[:success] = 'お気に入りを解除しました。'
     redirect_back(fallback_location: root_path)
   end
